1a2b3499812da15aa6a677cdae8a09c8be680c22
Brian Wilson <bwilson@oblong.com>
2016-05-24 10:05:10 -0400

    Who do you work for?

--- init.el
+++ init.el
@@ -372,6 +372,52 @@
 ;;   :init (auctex-skim-select))
 
 
+;; Oblong
+(defvar g-speak-home
+  (let ((g-speak-env (getenv "G_SPEAK_HOME")))
+    (if g-speak-env
+        g-speak-env
+      "/opt/oblong/g-speak3.24"))
+  "Location of g-speak root.")
+
+(defvar g-speak-deps
+  (let ((g-speak-deps-env (getenv "G_SPEAK_DEPS"))) ; TODO(brian): This can't be right...
+    (if g-speak-deps-env
+        g-speak-deps-env
+      "/opt/oblong/deps-64-11"))
+  "Location of g-speak deps.")
+
+(defun init/install-oblong-hooks ()
+  "Install any oblong specific hooks."
+  (let ((hook (lambda ()
+                (add-to-list 'flycheck-clang-include-path (concat g-speak-home "/include"))
+                (add-to-list 'flycheck-clang-include-path (concat g-speak-deps "/include")))))
+    (add-hook 'c++-mode-hook hook)
+    (add-hook 'c-mode-hook hook)))
+
+(defun init/set-oblong-persona ()
+  "This is my Oblong persona."
+  (progn
+    (setq user-mail-address "bwilson@oblong.com")
+    (setq calendar-latitude 42.34998)
+    (setq calendar-longitude -71.04949)
+    (setq calendar-location-name "Boston, MA")))
+
+(defun init/maybe-load-oblong ()
+  "Load oblong configs if on an oblong system."
+  (let ((oblong-dir (concat user-emacs-directory "oblong")))
+    (message (concat "Checking for oblong customizations @ " oblong-dir))
+    (when (file-exists-p oblong-dir)
+      (message "Oblong config required, loading...")
+      (init/set-oblong-persona)
+      (add-to-list 'load-path oblong-dir)
+      (load-file (concat oblong-dir "/init.el"))
+      (init/install-oblong-hooks)
+      (message "Oblong load complete."))))
+
+(init/maybe-load-oblong)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
