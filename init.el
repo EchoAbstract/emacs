60cfa96bc858c34a96706121cb209e9b7ab4548b
Brian Wilson <bwilson@oblong.com>
2017-05-23 16:13:51 -0400

    JS configuration

--- init.el
+++ init.el
@@ -338,7 +338,10 @@
 
 ;; Sort these
 (use-package go-mode :ensure t)
-(use-package js2-mode :ensure t)
+(use-package js2-mode :ensure t
+  :config (progn
+            (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+            (add-to-list 'interpreter-mode-alist '("node" . js2-mode))))
 (use-package ag :ensure t)
 (use-package web-mode :ensure t)
 (use-package yaml-mode :ensure t)
