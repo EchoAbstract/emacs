4461bb5774e86a614b7f0c399ac4d37e67843e55
Brian Wilson <bwilson@oblong.com>
2019-03-10 22:38:00 -0400

    WIP at emacs install

---
+++ install.el
@@ -0,0 +1,54 @@
+
+
+(defvar *pip-install-command*
+  "pip3 install"
+  "PIP3 command to install a dependency.")
+
+(defvar *pip-dependencies*
+  '("'python-language-server[all]'")
+  "Pip packages for python based dependencies.")
+
+(defvar *npm-install-command*
+  "npm i -g"
+  "NPM command to install a dependency.")
+
+(defvar *npm-dependencies*
+  '("bash-language-server"
+    "typescript"
+    "typescript-language-server"
+    "vscode-css-languageserver-bin"
+    "vscode-html-languageserver-bin"
+    "vue-language-server")
+  "NPM packages for node based dependencies.")
+
+(defvar *golang-install-command*
+  "go get -u"
+  "Go command to install a dependency.")
+
+(defvar *golang-dependencies*
+  '("github.com/saibing/bingo")
+  "Go based dependencies.")
+
+(defvar *macOS-install-command*
+  "brew install"
+  "macOS command to install a dependency.")
+
+(defvar *macOS-dependencies*
+  '("cquery")
+  "macOS specific dependencies.")
+
+(defvar *linux-install-command*
+  "sudo apt install"
+  "Linux (default Ubuntu/Debian) comamnd to install a dependency.")
+
+(defvar *linux-dependencies*
+  '()
+  "Linux specific dependencies.")
+
+(defun install-npm-deps ()
+  "Install all NPM based deps."
+  (mapcar (lambda (x) (shell-command (concat *npm-install-command* " " x))) *npm-dependencies*))
+
+(defun install-go-deps ()
+  "Install all Go lang based deps."
+  (mapcar (lambda (x) (shell-command (concat *golang-install-command* " " x))) *golang-dependencies*))
